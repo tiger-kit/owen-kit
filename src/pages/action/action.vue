735103766f9a14269d1073a9a9fb3caad72058c9
owen <ziwen_jiang@qq.com>
2023-09-21 21:28:33 +0800

    feat: action.vue add select button

--- src/pages/action/action.vue
+++ src/pages/action/action.vue
@@ -17,7 +17,7 @@
     <at-flex>
       <at-flex-item>
         <view class="create_new_habit">
-          创建新习惯  
+          创建新习惯
         </view>
       </at-flex-item>
     </at-flex>
@@ -29,26 +29,29 @@
           任务
         </view>
          <view class="task_right">
-           > 
+           >
         </view>
       </at-flex-item>
     </at-flex>
 
     <view class="task_list_cond">
       <view class="task_expire_sel">
-        <view class="task_expired"> 已逾期 </view>
-        <view class="task_future_do"> 以后再说 </view>
-        
+      <view class="btn_left">
+          <AtButton class="at_button" size='small' type='primary' @click="handleClick.bind(this, 'primary')">已逾期</AtButton>
+      </view> 
+      <view class="btn_right">
+        <AtButton class="at_button" size='small' type='primary' @click="handleClick.bind(this, 'primary')">以后再说</AtButton>
+      </view>
       </view>
     </view>
-    
+
   </view>
 
 </template>
 
 <script>
 
-import { AtFlex, AtFlexItem } from 'taro-ui-vue3'
+import { AtFlex, AtFlexItem, AtButton} from 'taro-ui-vue3'
 import { ref } from 'vue'
 import './action.scss'
 
